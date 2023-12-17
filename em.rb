de8300661d4dcd7b001e8df2cdf1d2826939c28d
Tuan Le <letuanvl03@gmail.com>
2023-12-17 14:01:40 +0700

    Add new file "em.rb" with class "Cnmoew" that outputs a message of love

---
+++ em.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Cnmoew < Princess
+  Mionguci = '
+     /\_/\
+    (=._.=)
+    / >❤️ <\
+  '
+  def love
+    puts Mionguci
+    puts 'Just love you!'
+  end
+end
